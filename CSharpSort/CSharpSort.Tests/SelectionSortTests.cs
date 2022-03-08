c6006a2fefad79b33a69297b5926031cfb6e3d82
Ahsan <ullahahsan1983@gmail.com>
2022-03-08 22:46:48 +0600

    Selection sort

---
+++ CSharpSort/CSharpSort.Tests/SelectionSortTests.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpSort.Tests;
+
+public class SelectionSortTests : UnitTestBase
+{
+    [Fact]
+    public void GivenNumericArray_WhenInvokedBySelectionSort_ThenProduceSortedArray()
+    {
+        var selection = new SelectionSort();
+
+        var arr = new[] { 20, 25, 15, 5, 10 };
+
+        selection.Sort(arr);
+
+        Assert.Equal(new[] { 5, 10, 15, 20, 25 }, arr);
+    }
+
+    [Theory]
+    // Best case
+    [InlineData(new[] { 5, 10, 15, 20, 25 }, new[] { 5, 10, 15, 20, 25 }, 10, 0, 15, 0)]
+    // Average case
+    [InlineData(new[] { 20, 25, 15, 5, 10 }, new[] { 5, 10, 15, 20, 25 }, 10, 2, 15, 4)]
+    // Worst case
+    [InlineData(new[] { 25, 20, 15, 10, 5 }, new[] { 5, 10, 15, 20, 25 }, 10, 2, 15, 4)]
+    public void WhenSortInvoked_ThenVerifyInsight(int[] arr, int[] expectedArray, long iterations, long placement, long reads, long writes)
+    {
+        var insight = SelectionSort.InsightfulSort(arr);
+
+        Assert.Equal(expectedArray, arr);
+        Assert.Equal(iterations, insight.Iterations);
+        Assert.Equal(reads, insight.Reads);
+        Assert.Equal(writes, insight.Writes);
+        Assert.Equal(placement, insight.KeyOps);
+    }
+}
---
+++ CSharpSort/CSharpSort/SelectionSort.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharpSort;
+
+public class SelectionSort : ISorting<int>
+{
+    private readonly Action? _onIterate;
+    private readonly Action? _onPlacement;
+    protected SelectionSort(Action onIterate, Action onPlacement)
+    {
+        _onIterate = onIterate;
+        _onPlacement = onPlacement;
+    }
+
+    public SelectionSort() { }
+
+    public void Sort(int[] arr)
+    {
+        var length = arr.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            var startValue = arr[i];
+            (var minValue, var minIndex) = (startValue, i);
+
+            for (int j = i + 1; j < length; j++)
+            {
+                _onIterate?.Invoke();
+                var value = arr[j];
+
+                if (value < minValue)
+                {
+                    minIndex = j;
+                    minValue = value;
+                }
+            }
+
+            if (minValue < startValue)
+            {
+                _onPlacement?.Invoke();
+                (arr[minIndex], arr[i]) = (startValue, minValue);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sort operation with operational insight
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static InsightData InsightfulSort(int[] arr)
+    {
+        (long iteration, long placement) = (0, 0);
+
+        var selection = new SelectionSort(() => iteration++, () => placement++);
+
+        selection.Sort(arr);
+
+        return new (iteration, iteration + arr.Length, placement * 2, placement);
+    }
+}
