e69da495dfca001ad206fb34a0d150e48bfc8227
Ahsan <ullahahsan1983@gmail.com>
2022-03-08 12:38:13 +0600

    Bubble sort

---
+++ CSharpSort/CSharpSort.Tests/BubbleSortTests.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpSort.Tests;
+
+public class BubbleSortTests : UnitTestBase
+{
+    [Fact]
+    public void GivenNumericArray_WhenInvokedByBubbleSort_ThenProduceSortedArray()
+    {
+        var bubble = new BubbleSort();
+
+        var arr = new[] { 20, 25, 15, 5, 10 };
+
+        bubble.Sort(arr);
+
+        Assert.Equal(new[] { 5, 10, 15, 20, 25 }, arr);
+    }
+
+    [Theory]
+    // Best case
+    [InlineData(new[] { 5, 10, 15, 20, 25 }, new[] { 5, 10, 15, 20, 25 }, 4, 0)]
+    // Average case
+    [InlineData(new[] { 20, 25, 15, 5, 10 }, new[] { 5, 10, 15, 20, 25 }, 10, 8)]
+    // Worst case
+    [InlineData(new[] { 25, 20, 15, 10, 5 }, new[] { 5, 10, 15, 20, 25 }, 10, 10)]
+    public void WhenSortInvoked_ThenVerifyInsight(int[] arr, int[] expectedArray, long expectedIterations, long expectedBubbles)
+    {
+        var bubble = new BubbleSort();
+
+        (long actualIterations, long actualBubbles) = bubble.InsightfulSort(arr);
+
+        Assert.Equal(expectedArray, arr);
+        Assert.Equal(expectedIterations, actualIterations);
+        Assert.Equal(expectedBubbles, actualBubbles);
+    }
+}
---
+++ CSharpSort/CSharpSort/BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿namespace CSharpSort;
+
+public class BubbleSort : ISorting<int>
+{
+    public void Sort(int[] arr)
+    {
+        SortCore(arr, null, null);
+    }
+
+    public (long iterations, long bubbles) InsightfulSort(int[] arr)
+    {
+        (long it, long b) = (0, 0);
+
+        SortCore(arr, () => it++, () => b++);
+
+        return (it, b);
+    }
+
+    static void SortCore(int[] arr, Action? iteration, Action? bubble)
+    {
+        var length = arr.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            bool bubbling = false;
+            for (int j = 1; j < length - i; j++)
+            {
+                bubbling |= BubbleUp(ref arr[j - 1], ref arr[j], iteration, bubble);
+            }
+
+            // if no bubbling happens in a full-round,
+            // then sorting is already completed
+            if (!bubbling) break;
+        }
+    }
+
+    static bool BubbleUp(ref int a, ref int b, Action? iteration, Action? bubble)
+    {
+        iteration?.Invoke();
+
+        if (a > b)
+        {
+            (b, a) = (a, b);
+            bubble?.Invoke();
+            return true;
+        }
+
+        return false;
+    }
+}
